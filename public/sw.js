dc9551aac7c0edafe62e6bcaa2f2262e2481396b
DheerajRay <dheerajray10@gmail.com>
2026-07-25 07:51:32 -0400

    Fix mobile PWA safe areas and slide height

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dheeraj-ray-presentation-v3";
+const CACHE = "dheeraj-ray-presentation-v4";
 const SHELL = ["/", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
